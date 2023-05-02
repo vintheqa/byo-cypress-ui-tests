8322cc304cbea93cdaf107749425e7ef889558f1
Marvin Colorado <marvin.colorado@achievewithoutborders.com>
2023-05-02 22:35:24 +0800

    outback changes

---
+++ cypress/support/pageElements/outbackPage.ts
@@ -0,0 +1,142 @@
+export const outbackPageElements = {
+
+  modelButton: 'a[href="/configure/trim-levels/AUOUT"]',
+  modelUrl: 'configure/trim-levels/AUOUT',
+ 
+  outbackVariants: [
+    "Outback AWD",
+    "Outback AWD Sport",
+    "Outback AWD Touring",
+    "Outback AWD Sport XT",
+    "Outback AWD Touring XT"
+  ],
+
+  outbackAwdColorOptions: [
+    "Crystal White Pearl",
+    "Ice Silver Metallic",
+    "Brilliant Bronze Metallic",
+    "Crimson Red Pearl",
+    "Storm Grey Metallic",
+    "Autumn Green Metallic",
+    "Sapphire Blue Pearl",
+    "Magnetite Grey Metallic",
+    "Crystal Black Silica",
+  ],
+
+  outbackAwdSportColorOptions: [
+    "Crystal White Pearl",
+    "Ice Silver Metallic",
+    "Autumn Green Metallic",
+    "Sapphire Blue Pearl",
+    "Magnetite Grey Metallic",
+    "Crystal Black Silica",
+  ],
+
+  outbackAwdTouringColorOptions: [
+    "Crystal White Pearl",
+    "Ice Silver Metallic",
+    "Brilliant Bronze Metallic",
+    "Crimson Red Pearl",
+    "Storm Grey Metallic",
+    "Autumn Green Metallic",
+    "Sapphire Blue Pearl",
+    "Magnetite Grey Metallic",
+    "Crystal Black Silica",
+  ],
+
+
+  outbackInteriorOptions: {
+    "awd" : ['Cloth - Black'],
+    "awdSport" : ['Water repellent^ - Grey and Black'],
+    "awdTouring" : [
+      'Nappa leather accented - Black',
+      'Nappa leather accented - Ivory and Black',
+      'Nappa leather accented - Tan (+ $590.00)'],
+    "awdSportXT" : ['Water repellent^ - Grey and Black'],
+    "awdTouringXT" : [
+      'Nappa leather accented - Black',
+      'Nappa leather accented - Ivory and Black'],
+  },
+
+  imgSrc: {
+    "awd" : [
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=1X&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=1U&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=CD&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=1Y&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=9Y&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=AP&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=CH&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=8Y&in=202&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25OA/?ex=4S&in=202&view=front'
+    ],
+    "awdSport": [
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25SA/?ex=1X&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25SA/?ex=1U&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25SA/?ex=AP&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25SA/?ex=CH&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25SA/?ex=8Y&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25SA/?ex=4S&in=402&view=front'
+    ],
+    "awdTouring" : [
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=1X&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=1U&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=CD&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=1Y&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=9Y&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=AP&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=CH&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=8Y&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW25TA/?ex=4S&in=205&view=front'
+    ],
+    "awdSportXt" : [
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TSA/?ex=1X&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TSA/?ex=1U&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TSA/?ex=AP&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TSA/?ex=CH&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TSA/?ex=8Y&in=402&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TSA/?ex=4S&in=402&view=front'
+    ],
+    "awdTouringXt" : [
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=1X&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=1U&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=CD&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=1Y&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=9Y&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=AP&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=CH&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=8Y&in=205&view=front',
+      'https://img.inchcapedigital.com/inchcosy/v2/subaruauasset/AUOW24TTA/?ex=4S&in=205&view=front'
+    ],
+  },
+
+  specsAndFeatureModalSubSections: [
+    'Measurement',
+    'Steering',
+    'Transmission',
+    'Symmetrical All-Wheel Drive',
+    'Suspension',
+    'Performance',
+    'Brakes',
+    'Engine',
+    'Wheels and Tyres',
+    'Interior Trims',
+    'Seating Extras',
+    'Entertainment',
+    'Collision Protection',
+    'Collision Avoidance',
+    'Security',
+    'Instrumentation and Controls',
+    'Exterior',
+    'Air-conditioning',
+    'Convenience',
+    'Intelligent Driving Dynamics',
+    'Storage',
+    'Lighting Exterior',
+    'Multi-Function Displays',
+    'Chassis and Mechanism',
+    'Child Safety',
+    'Safety Rating',
+  ],
+
+};
--- cypress/support/pageElements/urls.ts
+++ cypress/support/pageElements/urls.ts
@@ -2,5 +2,6 @@ export const urls = {
 wrxVariantSelectionPage:'https://www.subaru.com.au/configure/trim-levels/AUWRX/specificationPack?marketingCategory=Sedan&postcode=4000&transmissionType=gearbox-manual',
 imprezaVariantSelectionPage:'https://www.subaru.com.au/configure/trim-levels/AUIMP/specificationPack?marketingCategory=Sedan&postcode=4000',
 brzVariantSelectionPage:'https://www.subaru.com.au/configure/trim-levels/AUBRZ/specificationPack?marketingCategory=Manual&postcode=4000&transmissionType=gearbox-manual',
+outbackVariantSelectionPage:'https://www.subaru.com.au/configure/trim-levels/AUOUT/specificationPack?postcode=4000',
 crosstekVariantSelectionPage:'https://www.subaru.com.au/configure/trim-levels/AUWRX/specificationPack?marketingCategory=Sedan&postcode=4000&transmissionType=gearbox-manual',
 };
